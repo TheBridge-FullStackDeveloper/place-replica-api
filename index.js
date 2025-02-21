4bafdb7affffbcbc77ca59c6149c560657f32276
Girgetto <girgetto@users.noreply.github.com>
2025-02-21 18:55:08 +0100

    init

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const { createServer } = require('node:http');
+const { Server } = require('socket.io');
+const cors = require('cors');
+const port = 3000
+
+const app = express()
+var whitelist = ['*']
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+app.use(cors(corsOptions));
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+io.on('connection', (socket) => {
+  console.log('Un cliente se ha conectado');
+  socket.on('message', (msg) => {
+    io.emit('message', msg);
+  });
+
+  socket.on('place', (pixel) => {
+    io.emit('place', pixel);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Un cliente se ha desconectado');
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
